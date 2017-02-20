70415b77c6017202342441d0d65ceaf047b28ecc
Alex <Alex@alex>
2017-02-20 18:39:32 +0100

    a

--- aspectos-código/src/interfaces/VEscucharCancion.java
+++ aspectos-código/src/interfaces/VEscucharCancion.java
@@ -12,14 +12,27 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+
 import java.awt.Color;
 import java.awt.Font;
+import java.util.ArrayList;
+
 import javax.swing.JList;
 import javax.swing.JTextPane;
 
-public class VEscucharCanción extends JFrame {
+import modelo.Cancion;
+import modelo.Reproductor;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VEscucharCancion extends JFrame {
 
 	private JPanel contentPane;
+	private ArrayList<Cancion> laLista;
+	private boolean reproduciendo = true; 
+	private Reproductor rp = new Reproductor();
+	private int actual = 0;
 
 	/**
 	 * Launch the application.
@@ -28,7 +41,7 @@ public class VEscucharCanción extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VEscucharCanción frame = new VEscucharCanción();
+					VEscucharCancion frame = new VEscucharCancion();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,13 +52,24 @@ public class VEscucharCanción extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @throws Exception 
 	 */
 	
-	public VEscucharCanción() {
+	public VEscucharCancion() throws Exception {
+		
+		initialize();
+	}
+	
+	public void setListaCanciones(ArrayList<Cancion> lista){
+		laLista = lista;
+	}
+	
+	private void initialize() throws Exception {
 		setTitle("EUITI MUSIC PLAYER 3");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 605, 429);
 		contentPane = new JPanel();
+		setResizable(false);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -79,21 +103,81 @@ public class VEscucharCanción extends JFrame {
 		contentPane.add(labelNombreCanción);
 		
 		JButton btnPlayPause = new JButton("Play / Pause");
+		rp.AbrirFichero(laLista.get(actual).getRuta());
+		btnPlayPause.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(reproduciendo){
+					try {
+						rp.Stop();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}	
+				}else{
+					try {
+						rp.Play();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				reproduciendo=!reproduciendo;
+			}
+		});
 		btnPlayPause.setBounds(229, 256, 131, 23);
 		contentPane.add(btnPlayPause);
 		
 		JButton btnAnterior = new JButton("Anterior");
+		btnAnterior.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					rp.AbrirFichero(laLista.get(actual).getRuta());
+					rp.Play();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		actual--;
+		if(actual<0){
+			actual = laLista.size()-1;
+		}
 		btnAnterior.setBounds(130, 256, 89, 23);
+		if(laLista.size()==1){
+			btnAnterior.setEnabled(false);
+		}else{
+			btnAnterior.setEnabled(true);
+		}
 		contentPane.add(btnAnterior);
 		
 		JButton btnSiguiente = new JButton("Siguiente");
+		btnSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					rp.AbrirFichero(laLista.get(actual).getRuta());
+					rp.Play();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		actual++;
+		if(actual>laLista.size()-1){
+			actual = 0;
+		}
 		btnSiguiente.setBounds(370, 256, 89, 23);
+		if(laLista.size()==1){
+			btnSiguiente.setEnabled(false);
+		}else{
+			btnSiguiente.setEnabled(true);
+		}
 		contentPane.add(btnSiguiente);
-	
+		
+		JButton btnCompartir = new JButton("Compartir");
+		btnCompartir.setBounds(459, 45, 120, 23);
+		contentPane.add(btnCompartir);
+		
 		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setIcon(new ImageIcon(VEscucharCanción.class.getResource("/imagenes/imagen_play.png")));
+		lblNewLabel_1.setIcon(new ImageIcon(VEscucharCancion.class.getResource("/imagenes/imagen_play.png")));
 		lblNewLabel_1.setBounds(0, 0, 589, 390);
 		contentPane.add(lblNewLabel_1);
-		
-	}
+		}
 }
---
+++ aspectos-código/src/modelo/Reproductor.java
@@ -0,0 +1,30 @@
+package modelo;
+
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import java.io.File;
+
+public class Reproductor {
+
+	private BasicPlayer player = new BasicPlayer();
+	
+		public void Play() throws Exception {
+		  player.play();
+		}
+		 
+		public void AbrirFichero(String ruta) throws Exception {
+		  player.open(new File(ruta));
+		}
+		 
+		public void Pausa() throws Exception {
+		  player.pause();
+		}
+		 
+		public void Continuar() throws Exception {
+		  player.resume();
+		}
+		 
+		public void Stop() throws Exception {
+		  player.stop();
+		}
+	
+}
