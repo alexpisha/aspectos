4789fe8d1c8007e8b56c4c64e167fca3597aef19
Leire177 <Leire177@10.109.130.24>
2017-02-20 20:32:59 +0100

    cancion

--- aspectos-código/src/modelo/Cancion.java
+++ aspectos-código/src/modelo/Cancion.java
@@ -1,7 +1,11 @@
 package modelo;
 
 public class Cancion {
-
+	private int id;
+	private String titulo;
+	private String autor;
+	private String nombreRuta;
+	
 	public String getRuta() {
 		// TODO Auto-generated method stub
 		return null;
