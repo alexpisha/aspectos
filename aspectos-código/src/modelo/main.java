62fa300f84ceca588a76727c62b47458ba8925dd
Leire177 <Leire177@10.109.130.24>
2017-02-21 16:44:23 +0100

    a

--- aspectos-código/src/modelo/main.java
+++ aspectos-código/src/modelo/main.java
@@ -1,10 +1,13 @@
 package modelo;
 
+import interfaces.VPrincipal;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SGBD.getSGBD().ingresarJugador("Leire", "123");
+		VPrincipal v = new VPrincipal();
+		v.setVisible(true);
 	}
 
 }
